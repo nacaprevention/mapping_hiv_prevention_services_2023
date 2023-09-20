33c702ca36e4bcbeba286610342e13866606f67b
Damilola Iyiola <dkiyiola@gmail.com>
2023-09-20 16:24:26 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Mapping HIV Prevention Services, Nigeria, 2023`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
